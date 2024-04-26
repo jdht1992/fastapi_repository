d99c10424927a04140fadeaa1ee560c9eec4d3d0
Diego H <jdht1992@gmail.com>
2024-04-26 11:08:56 -0600

    database and image configuration

---
+++ config/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config.config import get_settings
+
+
+settings = get_settings()
+
+DB_URL = f"postgresql://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@locahost:5436/{settings.DB_NAME}"
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    """
+    Create a database session.
+    Yields:
+        Session: The database session.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
