b7c218e4169679eb1ac3d0bb0bc6b400a8d7b580
Diego H <jdht1992@gmail.com>
2024-04-26 12:19:52 -0600

    added models of database

---
+++ models/city.py
@@ -0,0 +1,14 @@
+import uuid
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+from config.database import Base
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    region_id = Column(UUID(as_uuid=True), ForeignKey("region.id"))
+    region = relationship("Region", back_population="cities")
