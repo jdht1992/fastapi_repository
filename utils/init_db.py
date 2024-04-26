c4740f38d8739cfda7a82dffb3edfb783d4a43a0
Diego H <jdht1992@gmail.com>
2024-04-26 12:41:17 -0600

    added schemas for models

---
+++ utils/init_db.py
@@ -0,0 +1,12 @@
+from config.database import engine
+from models.city import City
+from models.region import Region
+
+
+def create_tables():
+    """
+    Creates all database tables defined in the application.
+    """
+    City.metadata.create_all(bind=engine)
+    Region.metadata.create_all(bind=engine)
+    
